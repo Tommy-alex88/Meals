df7bdc20290bbfb96226711fcd06ffceb8267d5b
Alexander <tommy.alex88@gmail.com>
2021-07-28 13:56:32 +0700

    MealsItemsList added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+
+import { Modal } from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onBackdropClick={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
